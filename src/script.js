ab33ac8a81055edd36932dc024329ef7d8f7d4cf
ylfyt <ayudi3521@gmail.com>
2023-04-03 21:04:55 +0700

    Control Youtube Volume

--- src/script.js
+++ src/script.js
@@ -27,6 +27,36 @@ const prev = (tabId) => {
 	});
 };
 
+const up = (tabId) => {
+	const id = tabId.substring(2, tabId.length);
+
+	chrome.scripting.executeScript({
+		func: () => {
+			const video = document.getElementById('movie_player');
+			video.setVolume(video.getVolume() + 5);
+		},
+		target: {
+			tabId: parseInt(id),
+		},
+		world: 'MAIN',
+	});
+};
+
+const down = (tabId) => {
+	const id = tabId.substring(2, tabId.length);
+
+	chrome.scripting.executeScript({
+		func: () => {
+			const video = document.getElementById('movie_player');
+			video.setVolume(video.getVolume() - 5);
+		},
+		target: {
+			tabId: parseInt(id),
+		},
+		world: 'MAIN',
+	});
+};
+
 const next = (tabId) => {
 	const id = tabId.substring(2, tabId.length);
 	chrome.scripting.executeScript({
@@ -93,6 +123,9 @@ const getHtmlForYT = (tab, prev = false) => {
         <button id="s:${tab.id}" type="button" class="control prev ${prev ? 'visible' : 'invisible'}">&#10094;</button>
         <button id="p:${tab.id}" type="button" class="play-toggle control">&#9737;</button>
         <button id="n:${tab.id}" type="button" class="control next">&#10095;</button>
+
+        <button id="d:${tab.id}" type="button" class="control volume-down">-</button>
+        <button id="u:${tab.id}" type="button" class="control volume-up">+</button>
       </div>
 			<button id="${tab.id}" type="button" class="mute-toggle control">&#9888;</button>
 		</div>
@@ -105,7 +138,7 @@ const tabsContainer = document.getElementById('tabs');
 chrome.tabs.query({}, function (tabs) {
 	for (let i = 0; i < tabs.length; i++) {
 		const tab = tabs[i];
-		if (tab.url.includes('youtube.com/watch?v=')) {
+		if (tab.url.includes('youtube.com/watch?')) {
 			if (tab.url.includes('list=')) {
 				tabsContainer.innerHTML += getHtmlForYT(tab, true);
 			} else {
@@ -145,6 +178,22 @@ chrome.tabs.query({}, function (tabs) {
 		const button = prevButtons[i];
 		button.addEventListener('click', () => {
 			prev(button.id);
+		});
+	}
+
+	const volumeUpButtons = document.getElementsByClassName('volume-up');
+	for (let i = 0; i < volumeUpButtons.length; i++) {
+		const button = volumeUpButtons[i];
+		button.addEventListener('click', () => {
+			up(button.id);
+		});
+	}
+
+	const volumeDownButtons = document.getElementsByClassName('volume-down');
+	for (let i = 0; i < volumeDownButtons.length; i++) {
+		const button = volumeDownButtons[i];
+		button.addEventListener('click', () => {
+			down(button.id);
 		});
 	}
 });
